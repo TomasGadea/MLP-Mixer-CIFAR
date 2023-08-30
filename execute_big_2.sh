ec9bd0e35592f88383eaa23c1972ef34d6fb368e
TomasGadea <01tomas.gadea@gmail.com>
2023-08-30 17:36:39 +0200

    syncccc

--- execute_big_2.sh
+++
@@ -1,3 +0,0 @@
-export WANDB_API_KEY="21d72275648132f2136abaa14ddd913550563d16"
-source environ/bin/activate
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 python main.py --dataset c10 --model mlp_mixer --autoaugment --cutmix-prob 0.5 --seed 123456 --hidden-size 512 --hidden-c 2048 --hidden-s 256 --batch-size 512 --num-layers 30
--- execute_small_4.sh
+++
@@ -1,3 +0,0 @@
-export WANDB_API_KEY="21d72275648132f2136abaa14ddd913550563d16"
-source environ/bin/activate
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 python main.py --dataset c10 --model mlp_mixer --autoaugment --cutmix-prob 0.5 --seed 123456 --hidden-size 16 --hidden-c 64 --hidden-s 8 --batch-size 128 --num-layers 2
